1ac0c49f769791350da8b01ffa36aaa9df523025
acap-ipin <asyraf.arifin@openintegration.com>
2021-05-24 14:57:14 +0800

    done xml file creation

--- CppCLR_WinformsProjekt2/FlowForm.h
+++ CppCLR_WinformsProjekt2/FlowForm.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <fstream>
+#include <vcclr.h>
 namespace CppCLRWinformsProjekt2 {
 
 	using namespace System;
@@ -66,6 +67,7 @@ namespace CppCLRWinformsProjekt2 {
 
 
 
+
 	private:
 		/// <summary>
 		/// Erforderliche Designervariable.
@@ -82,9 +84,9 @@ namespace CppCLRWinformsProjekt2 {
 			this->labelFlowName = (gcnew System::Windows::Forms::Label());
 			this->textBoxFlowName = (gcnew System::Windows::Forms::TextBox());
 			this->groupBoxProcessFlow = (gcnew System::Windows::Forms::GroupBox());
-			this->dataGridViewFlows = (gcnew System::Windows::Forms::DataGridView());
 			this->buttonFlowSave = (gcnew System::Windows::Forms::Button());
 			this->buttonRowAdd = (gcnew System::Windows::Forms::Button());
+			this->dataGridViewFlows = (gcnew System::Windows::Forms::DataGridView());
 			this->groupBoxProcessFlow->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewFlows))->BeginInit();
 			this->SuspendLayout();
@@ -124,24 +126,6 @@ namespace CppCLRWinformsProjekt2 {
 			this->groupBoxProcessFlow->TabStop = false;
 			this->groupBoxProcessFlow->Text = L"Process Flow";
 			// 
-			// dataGridViewFlows
-			// 
-			this->dataGridViewFlows->AllowUserToAddRows = false;
-			this->dataGridViewFlows->AllowUserToDeleteRows = false;
-			this->dataGridViewFlows->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
-				| System::Windows::Forms::AnchorStyles::Left)
-				| System::Windows::Forms::AnchorStyles::Right));
-			this->dataGridViewFlows->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridViewFlows->Location = System::Drawing::Point(20, 29);
-			this->dataGridViewFlows->Name = L"dataGridViewFlows";
-			this->dataGridViewFlows->RowHeadersVisible = false;
-			this->dataGridViewFlows->RowHeadersWidth = 51;
-			this->dataGridViewFlows->RowTemplate->Height = 24;
-			this->dataGridViewFlows->Size = System::Drawing::Size(1126, 371);
-			this->dataGridViewFlows->TabIndex = 8;
-			this->dataGridViewFlows->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Form2::dataGridViewFlows_CellClick);
-			this->dataGridViewFlows->CellValueChanged += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Form2::dataGridViewFlows_ValueChanged);
-			// 
 			// buttonFlowSave
 			// 
 			this->buttonFlowSave->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
@@ -171,6 +155,26 @@ namespace CppCLRWinformsProjekt2 {
 			this->buttonRowAdd->Text = L"Add Row";
 			this->buttonRowAdd->UseVisualStyleBackColor = false;
 			this->buttonRowAdd->Click += gcnew System::EventHandler(this, &Form2::buttonRowAdd_Click);
+			// 
+			// dataGridViewFlows
+			// 
+			this->dataGridViewFlows->AllowUserToAddRows = false;
+			this->dataGridViewFlows->AllowUserToDeleteRows = false;
+			this->dataGridViewFlows->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->dataGridViewFlows->BackgroundColor = System::Drawing::SystemColors::ButtonFace;
+			this->dataGridViewFlows->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridViewFlows->Location = System::Drawing::Point(20, 29);
+			this->dataGridViewFlows->Name = L"dataGridViewFlows";
+			this->dataGridViewFlows->RowHeadersVisible = false;
+			this->dataGridViewFlows->RowHeadersWidth = 51;
+			this->dataGridViewFlows->RowTemplate->Height = 24;
+			this->dataGridViewFlows->Size = System::Drawing::Size(1126, 371);
+			this->dataGridViewFlows->TabIndex = 8;
+			this->dataGridViewFlows->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Form2::dataGridViewFlows_CellClick);
+			this->dataGridViewFlows->CellValueChanged += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &Form2::dataGridViewFlows_ValueChanged);
+			this->dataGridViewFlows->CurrentCellDirtyStateChanged += gcnew System::EventHandler(this, &Form2::dataGridViewFlows_CurrentCellDirtyStateChanged);
 			// 
 			// Form2
 			// 
@@ -209,31 +213,39 @@ namespace CppCLRWinformsProjekt2 {
 			colAction->Name = "colAction";
 			colAction->Items->AddRange("Update", "Trigger", "Add");
 			colAction->Width = 90;
+			colAction->FlatStyle = FlatStyle::Flat;
 
 			colItems->HeaderText = "Items";
 			colItems->Name = "colItems";
 			//colItems->Items->AddRange("SV", "EC", "Event", "Alarm", "Delay Timer", "Wait for cmd");
 			colItems->Width = 90;
+			colItems->FlatStyle = FlatStyle::Flat;
+			colItems->DefaultCellStyle->BackColor = System::Drawing::Color::Gray;
 
 			colName->HeaderText = "Name";
 			colName->Name = "colName";
 			colName->Width = 200;
+			colName->ReadOnly = true;
+			colName->DefaultCellStyle->BackColor = System::Drawing::Color::Gray;
 
 			colValue->HeaderText = "Value";
 			colValue->Name = "colValue";
 			colValue->AutoSizeMode = System::Windows::Forms::DataGridViewAutoSizeColumnMode::Fill;
+			colValue->ReadOnly = true;
+			colValue->DefaultCellStyle->BackColor = System::Drawing::Color::Gray;
 
 			colFormat->HeaderText = "Format";
 			colFormat->Name = "colFormat";
-			colFormat->Items->AddRange("A", "B", "U4");
 			colFormat->Width = 70;
+			colFormat->FlatStyle = FlatStyle::Flat;
+			colFormat->DefaultCellStyle->BackColor = System::Drawing::Color::Gray;
 
-			colAddButton->Name = "colAddButton";
+			/*colAddButton->Name = "colAddButton";
 			colAddButton->UseColumnTextForButtonValue = true;
 			colAddButton->Text = "Add";
 			colAddButton->HeaderText = "";
 			colAddButton->Width = 70;
-			colAddButton->DefaultCellStyle->BackColor = System::Drawing::Color::Lime;
+			colAddButton->DefaultCellStyle->BackColor = System::Drawing::Color::Lime;*/
 
 			colRemoveButton->Name = "colRemoveButton";
 			colRemoveButton->UseColumnTextForButtonValue = true;
@@ -258,40 +270,219 @@ namespace CppCLRWinformsProjekt2 {
 			this->dataGridViewFlows->Rows->Add();
 		}
 
-		private: System::Void buttonFlowSave_Click(System::Object^ sender, System::EventArgs^ e) {
-		}
 		private: System::Void dataGridViewFlows_CellClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
-			if (e->ColumnIndex == 9) {
-				Object^ actionCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[0]->Value;
-				if (actionCell != nullptr) {
-					MessageBox::Show("Clicked" + actionCell->ToString(), "Error", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
-				}
-			}
-			else if(e->ColumnIndex == 5){
+			if(e->ColumnIndex == 5){
 				this->dataGridViewFlows->Rows->RemoveAt(e->RowIndex);
 			}
 		}
 
-		private: System::Void dataGridViewFlows_ValueChanged(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
-			Object^ actionCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[0]->Value;
-			if (actionCell != nullptr) {
-				DataGridViewCell^ itemsCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[0];
-				DataGridViewComboBoxCell^ itemsCombobox = (DataGridViewComboBoxCell^)itemsCell;
-				this->dataGridViewFlows->Rows[e->RowIndex]->Cells[1]->Value = nullptr;
+		private: System::Void dataGridViewFlows_CurrentCellDirtyStateChanged(System::Object^ sender, System::EventArgs^ e) {
+			if (dataGridViewFlows->IsCurrentCellDirty) {
+				dataGridViewFlows->CommitEdit(DataGridViewDataErrorContexts::Commit);
+			}
+		}
 
-				if (actionCell->ToString()->Equals("Update")) {
-					itemsCombobox->Items->AddRange("SV", "EC");
+		private: System::Void dataGridViewFlows_ValueChanged(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+			//check if the changed value is the first column
+			if (e->ColumnIndex == 0) {
+				Object^ actionCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[0]->Value;
+				DataGridViewCell^ itemsCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[1];
+				if (actionCell != nullptr) {
+					itemsCell->Style->BackColor = System::Drawing::Color::White;
+					DataGridViewComboBoxCell^ itemsCombobox = (DataGridViewComboBoxCell^)itemsCell;
+					//first, clear away the items combobox dropdown options and selected value
+					while (itemsCombobox->Items->Count != 0){
+						itemsCombobox->Items->RemoveAt(0);
+					}
+					itemsCell->Value = nullptr;
+
+					if (actionCell->ToString()->Equals("Update")) {
+						itemsCombobox->Items->AddRange("SV", "EC");
+					}
+					else if (actionCell->ToString()->Equals("Trigger")) {
+						itemsCombobox->Items->AddRange("Event", "Alarm");
+					}
+					else if (actionCell->ToString()->Equals("Add")) {
+						itemsCombobox->Items->AddRange("Delay timer", "Await cmd");
+					}
 				}
-				else if (actionCell->ToString()->Equals("Trigger")) {
-					//itemsCombobox->Items->Clear();
-					itemsCombobox->Items->AddRange("SV", "EC");
+				else {
+					itemsCell->Style->BackColor = System::Drawing::Color::Gray;
 				}
-				else if (actionCell->ToString()->Equals("Add")) {
-					//itemsCombobox->Items->Clear();
-					itemsCombobox->Items->AddRange("SV", "EC");
+			}
+
+			//check if changed value is the 2nd column
+			if (e->ColumnIndex == 1) {
+				Object^ itemsCell = this->dataGridViewFlows->Rows[e->RowIndex]->Cells[1]->Value;
+				if (itemsCell != nullptr) {
+					DataGridViewTextBoxCell^ nameCell = (DataGridViewTextBoxCell^) this->dataGridViewFlows->Rows[e->RowIndex]->Cells[2];
+					DataGridViewTextBoxCell^ valueCell = (DataGridViewTextBoxCell^) this->dataGridViewFlows->Rows[e->RowIndex]->Cells[3];
+					DataGridViewComboBoxCell^ formatCell = (DataGridViewComboBoxCell^)this->dataGridViewFlows->Rows[e->RowIndex]->Cells[4];
+					if (itemsCell->ToString()->Equals("SV")) {
+						nameCell->ReadOnly = false;
+						nameCell->Style->BackColor = System::Drawing::Color::White;
+
+						valueCell->ReadOnly = false;
+						valueCell->Style->BackColor = System::Drawing::Color::White;
+
+						formatCell->Items->AddRange("A", "B", "U4");
+						formatCell->Style->BackColor = System::Drawing::Color::White;
+					}
+					else if (itemsCell->ToString()->Equals("EC")) {
+						nameCell->ReadOnly = false;
+						nameCell->Style->BackColor = System::Drawing::Color::White;
+
+						valueCell->ReadOnly = false;
+						valueCell->Style->BackColor = System::Drawing::Color::White;
+
+						while (formatCell->Items->Count != 0) {
+							formatCell->Items->RemoveAt(0);
+						}
+						formatCell->Style->BackColor = System::Drawing::Color::Gray;
+					}
+					else if (itemsCell->ToString()->Equals("Event")) {
+						nameCell->ReadOnly = false;
+						nameCell->Style->BackColor = System::Drawing::Color::White;
+
+						valueCell->ReadOnly = true;
+						valueCell->Style->BackColor = System::Drawing::Color::Gray;
+
+						while (formatCell->Items->Count != 0) {
+							formatCell->Items->RemoveAt(0);
+						}
+						formatCell->Style->BackColor = System::Drawing::Color::Gray;
+					}
+					else if (itemsCell->ToString()->Equals("Alarm")) {
+						nameCell->ReadOnly = false;
+						nameCell->Style->BackColor = System::Drawing::Color::White;
+
+						valueCell->ReadOnly = true;
+						valueCell->Style->BackColor = System::Drawing::Color::Gray;
+
+						while (formatCell->Items->Count != 0) {
+							formatCell->Items->RemoveAt(0);
+						}
+						formatCell->Style->BackColor = System::Drawing::Color::Gray;
+					}
+					else if (itemsCell->ToString()->Equals("Delay timer")) {
+						nameCell->ReadOnly = true;
+						nameCell->Style->BackColor = System::Drawing::Color::Gray;
+
+						valueCell->ReadOnly = false;
+						valueCell->Style->BackColor = System::Drawing::Color::White;
+
+						while (formatCell->Items->Count != 0) {
+							formatCell->Items->RemoveAt(0);
+						}
+						formatCell->Style->BackColor = System::Drawing::Color::Gray;
+					}
+					else if (itemsCell->ToString()->Equals("Await cmd")) {
+						nameCell->ReadOnly = false;
+						nameCell->Style->BackColor = System::Drawing::Color::White;
+
+						valueCell->ReadOnly = true;
+						valueCell->Style->BackColor = System::Drawing::Color::Gray;
+
+						while (formatCell->Items->Count != 0) {
+							formatCell->Items->RemoveAt(0);
+						}
+						formatCell->Style->BackColor = System::Drawing::Color::Gray;
+					}
 				}
 			}
 		}
+
+		private: std::string convertSystemStringToBasicString(String^ str) {
+			pin_ptr<const wchar_t> wch = PtrToStringChars(str);
+			std::wstring wstr(wch);
+			std::string mainStr(wstr.begin(), wstr.end());
+			return mainStr;
+		}
+
+		private: System::Void buttonFlowSave_Click(System::Object^ sender, System::EventArgs^ e) {
+			int flowsCount = this->dataGridViewFlows->Rows->Count;
+			bool valid = true;
+
+			for (int i = 0; i < flowsCount; i++) {
+				Object^ actionObj = this->dataGridViewFlows->Rows[i]->Cells[0]->Value;
+				Object^ itemsObj = this->dataGridViewFlows->Rows[i]->Cells[1]->Value;
+				Object^ nameObj = this->dataGridViewFlows->Rows[i]->Cells[2]->Value;
+				Object^ valueObj = this->dataGridViewFlows->Rows[i]->Cells[3]->Value;
+				Object^ formatObj = this->dataGridViewFlows->Rows[i]->Cells[4]->Value;
+				
+				if (actionObj != nullptr && itemsObj != nullptr) {
+					if (itemsObj->Equals("SV")) {
+						if (nameObj == nullptr || valueObj == nullptr || formatObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+					else if (itemsObj->Equals("EC")) {
+						if (nameObj == nullptr || valueObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+					else if (itemsObj->Equals("Event")) {
+						if (nameObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+					else if (itemsObj->Equals("Alarm")) {
+						if (nameObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+					else if (itemsObj->Equals("Delay timer")) {
+						if (valueObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+					else if (itemsObj->Equals("Await cmd")) {
+						if (nameObj == nullptr) {
+							valid = false;
+							break;
+						}
+					}
+				}
+				else {
+					valid = false;
+				}
+			}
+
+			if (!valid) {
+				MessageBox::Show("Incomplete flow. Make sure fill in all required cells", "Error", MessageBoxButtons::OK, MessageBoxIcon::Exclamation);
+			}
+			else {
+				std::ofstream file;
+
+				String^ flowname = this->textBoxFlowName->Text;
+				String^ filepath = "LocalData/Flows/" + flowname + ".xml";
+				file.open(convertSystemStringToBasicString(filepath));
+				file << "<?xml version=\"1.0\" encoding=\"utf-8\"?><Flows>";
+
+				for (int i = 0; i < flowsCount; i++) {
+					Object^ actionObj = this->dataGridViewFlows->Rows[i]->Cells[0]->Value;
+					Object^ itemsObj = this->dataGridViewFlows->Rows[i]->Cells[1]->Value;
+					Object^ nameObj = this->dataGridViewFlows->Rows[i]->Cells[2]->Value;
+					Object^ valueObj = this->dataGridViewFlows->Rows[i]->Cells[3]->Value;
+					Object^ formatObj = this->dataGridViewFlows->Rows[i]->Cells[4]->Value;
+					String^ writeln = "\r\n<Flow Action=\"" + actionObj 
+						+ "\" Item=\"" + itemsObj 
+						+ "\" Name=\"" + nameObj 
+						+ "\" Value=\"" + valueObj
+						+ "\" Format=\"" + formatObj
+						+ "\"></Flow>";
+					file << convertSystemStringToBasicString(writeln);
+				}
+				file << "</Flows>";
+				file.close();
+			}
+		}
 	
-	};//stupid IDE forcing wrong indentation here
+	
+};//stupid IDE forcing wrong indentation here
 }
